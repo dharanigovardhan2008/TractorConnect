5d3a3656b2439a85241cfc36be7a5bd658561593
dharanigovardhan2008 <palerugopi00@gmail.com>
2026-05-23 13:26:46 +0530

    Fixed browser cache issue

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: __dirname,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0",
+          },
+        ],
+      },
+    ];
   },
 };
 
